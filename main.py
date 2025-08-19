abede0f999be754b136823cb6504445bdec3e47e
wowislive <wowislive@gmail.com>
2025-08-19 17:04:34 +0000

    circlesshape.py added

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -11,7 +14,7 @@ def main():
             
         screen.fill("black")
         pygame.display.flip()
-
-
+        dt = clock.tick(60) / 1000
+        
 if __name__ == "__main__":
     main()
