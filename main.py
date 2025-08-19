8f6611e2c6e05d21c81978bd3714f79dfba41582
wowislive <wowislive@gmail.com>
2025-08-19 22:30:38 +0000

    added player rotation feature

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ def main():
                 return
             
         screen.fill("black")
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
