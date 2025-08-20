2c85c33f27743c54a275c8917e9cecf8062b982f
wowislive <wowislive@gmail.com>
2025-08-20 13:36:59 +0000

    added timer to calibrate fire rate

--- player.py
+++ player.py
@@ -7,6 +7,7 @@ class Player(CircleShape):
   def __init__(self, x, y):
     super().__init__(x, y, PLAYER_RADIUS)
     self.rotation = 180
+    self.timer = 0
     
   def triangle(self):
     forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -27,6 +28,7 @@ class Player(CircleShape):
     self.position += forward * PLAYER_SPEED * dt
 
   def update(self, dt):
+    self.timer -= dt
     keys = pygame.key.get_pressed()
 
     if keys[pygame.K_a]:
@@ -41,5 +43,10 @@ class Player(CircleShape):
       self.shoot()
 
   def shoot(self):
+    if self.timer > 0:
+      return
+    
     new_shot = Shot(self.position.x, self.position.y)
     new_shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+    self.timer = PLAYER_SHOOT_COOLDOWN
+    
